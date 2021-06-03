2ff2291d0200773a9e9c9415b63e89f1a2424440
lukostelloe <lukostelloe@gmail.com>
2021-06-03 16:48:30 +0200

    more morpion

--- morpion/morpion.js
+++ morpion/morpion.js
@@ -2,6 +2,8 @@ let cells = document.querySelectorAll(".cell");
 let instr = document.getElementById("game-status");
 let arrays = new Array(9);
 let player1 = 1;
+
+const playagainButton = document.getElementById("playagain");
 // document.getElementById("0").textContent = "T";
 
 // let items = Array.from(cells).map((elem) => {
@@ -31,14 +33,53 @@ for (i = 0; i < cells.length; i++) {
     j = parseInt(e.target.id);
     if (player1 == 1) {
       e.target.innerHTML = "X";
+      e.target.disabled = "true";
+      e.target.style.cursor = "not-allowed";
       instr.innerHTML = "Player 2's turn";
       arrays[j] = 1;
     } else {
       e.target.innerHTML = "O";
+      e.target.disabled = "true";
+      e.target.style.cursor = "not-allowed";
       instr.innerHTML = "Player 1's turn";
       arrays[j] = 0;
     }
+    checkWin();
     switchPlayer();
     console.log(player1);
   };
 }
+
+function checkWin() {
+  if (
+    (arrays[0] == 1 && arrays[1] == 1 && arrays[2] == 1) || ///////X HORIZONTAL///////////
+    (arrays[3] == 1 && arrays[4] == 1 && arrays[5] == 1) ||
+    (arrays[6] == 1 && arrays[7] == 1 && arrays[8] == 1) ||
+    (arrays[0] == 1 && arrays[3] == 1 && arrays[6] == 1) || ////////X VERTICAL////////////
+    (arrays[1] == 1 && arrays[4] == 1 && arrays[7] == 1) ||
+    (arrays[2] == 1 && arrays[5] == 1 && arrays[8] == 1) ||
+    (arrays[0] == 1 && arrays[4] == 1 && arrays[8] == 1) || /////////X DIAGONAL/////////////
+    (arrays[2] == 1 && arrays[4] == 1 && arrays[6] == 1)
+  ) {
+    instr.innerHTML = "Player 1 wins! Play again?";
+    playagainButton.style.display = "block";
+  } else if (
+    (arrays[0] == 0 && arrays[1] == 0 && arrays[2] == 0) || ///////X HORIZONTAL///////////
+    (arrays[3] == 0 && arrays[4] == 0 && arrays[5] == 0) ||
+    (arrays[6] == 0 && arrays[7] == 0 && arrays[8] == 0) ||
+    (arrays[0] == 0 && arrays[3] == 0 && arrays[6] == 0) || ////////X VERTICAL////////////
+    (arrays[1] == 0 && arrays[4] == 0 && arrays[7] == 0) ||
+    (arrays[2] == 0 && arrays[5] == 0 && arrays[8] == 0) ||
+    (arrays[0] == 0 && arrays[4] == 0 && arrays[8] == 0) || /////////X DIAGONAL/////////////
+    (arrays[2] == 0 && arrays[4] == 0 && arrays[6] == 0)
+  ) {
+    instr.innerHTML = "Player 2 wins! Play again?";
+    playagainButton.style.display = "block";
+  }
+}
+
+function restart() {
+  window.location.reload();
+}
+
+playagainButton.addEventListener("click", restart);
